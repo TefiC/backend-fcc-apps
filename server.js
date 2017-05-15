d8ddb15083b40e2224143cc71a20da089179ddde
Estefania Cass <TefiC@users.noreply.github.com>
2017-05-15 17:10:12 +0000

    Added image search project

--- server.js
+++ server.js
@@ -272,7 +272,116 @@ app.post('/uploads', upload.single('file-input'), function(req, res) {
  * IMAGE SEARCH ABSTRACTION LAYER
  * -------------------------------------------------------*/
 
-// TODO: build this feature
+var request = require("request");
+
+var dataURL = process.env.MONGOLAB_URI_2;
+
+var apiKey = process.env.GOOGLESEARCHKEY;
+var searchEngineID = process.env.SEARCHENGINEID;
+
+app.get('/image-search-abstraction-layer/', function(req, res) {
+	res.sendFile(__dirname + '/index/image-search-index.html');	
+});
+
+
+app.get('/image-search-abstraction-layer/new-search/*', function (req, res) {
+	
+	res.writeHead(200, {'content-type':'application/JSON'});
+	
+	var searchQuery = req.params[0];
+	var searchType = 'image';
+	var offset = req.query["offset"];
+	
+	var requestURL = 'https://www.googleapis.com/customsearch/v1?key=' + apiKey + 
+					'&cx=' + searchEngineID + 
+					'&q=' + searchQuery + 
+					'&searchType=' + searchType + 	
+					'&num=' + offset
+
+					
+	//Add the search to our database for recent searches				
+	mongo.connect(dataURL, function(err, db) {
+		
+		if (err) throw err;
+	   
+		var recentSearchCollection = db.collection('recentSearch');
+		
+		var date = new Date().toJSON();
+	   
+		recentSearchCollection.insert({
+			"term": searchQuery,
+			"date": date
+		}, function(err, data) {
+			console.log('Search added succesfully!');
+			if (err) throw err;
+			db.close();
+		});
+	});
+
+	
+	request( {uri: requestURL, json:true}, function(error, response, body) {
+		
+		if (error) {
+			
+			res.end('There was an error with your request');
+		
+		} else {
+			
+			var imagesResponse = body["items"];
+		
+			var data = []
+		
+			var i;
+		
+			for (i=0; i<imagesResponse.length; i++) {
+			
+				var image = imagesResponse[i]
+			
+				data.push({
+					"url": image["link"],
+					"snippet": image["snippet"],
+					"thumbnail": image["image"]["thumbnailLink"],
+					"context": image["image"]["contextLink"]
+				});
+			} 
+		
+			res.end(JSON.stringify(data));
+		}
+		
+	});
+	
+}); 
+
+app.get('/image-search-abstraction-layer/recent-searches/', function(req, res) {
+	
+	res.writeHead(200, {'content-type':'application/JSON'});
+	
+	//Add the search to our database for recent searches				
+	mongo.connect(dataURL, function(err, db) {
+		
+		if (err) throw err;
+	   
+		var recentSearchCollection = db.collection('recentSearch');
+	   
+		recentSearchCollection.find().limit(10).sort({_id:-1}).toArray(function(err, results){
+			
+			if (err) throw err;
+			
+			var data = []
+			
+			results.forEach(function(result) {
+				data.push({
+					"term": result["term"],
+					"when": result["date"]
+				});
+			});
+			
+			res.end(JSON.stringify(data));
+		});
+
+	});
+
+});
 
 
 /*
